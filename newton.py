8f707d962438488c25bb54f16c4ec21abecf81e3
Eavan Pattie <e.pattie@warwick.ac.uk>
2020-02-27 12:07:27 +0000

    add pure newton

--- newton.py
+++ newton.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+def pure_newton(func, grad, hessian, initial, tolerance, max_steps):
+    iterations = 0
+    x = initial
+
+    while grad(x).dot(grad(x)) > tolerance ** 2 and iterations < max_steps:
+        direction = np.linalg.solve(hessian(x), -grad(x))
+        x += direction
+
+    return iterations, x
